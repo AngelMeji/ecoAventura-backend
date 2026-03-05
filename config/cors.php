1e169595895cbf34d47f1218a39298510b2f72b1
AngelMeji <joseangelmejiamedina@gmail.com>
2026-03-05 08:56:57 -0500

    arreglo de coors

--- config/cors.php
+++ config/cors.php
@@ -19,12 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',  // Vite dev server (default)
-        'http://localhost:3000',  // También por si acaso
-        'http://localhost:4173',  // Vite preview
-        env('FRONTEND_URL', 'http://localhost:5173'),
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -34,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
